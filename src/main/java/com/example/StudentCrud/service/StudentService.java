90eeb9680218e725e18498e308cbcfb24f956792
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-03-05 14:35:16 +0530

    StudentCrud

---
+++ src/main/java/com/example/StudentCrud/service/StudentService.java
@@ -0,0 +1,37 @@
+package com.example.StudentCrud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.StudentCrud.domain.Student;
+import com.example.StudentCrud.repository.StudentRepository;
+
+@Service
+public class StudentService {
+	
+	
+	@Autowired
+	private StudentRepository repo;
+	
+	public List<Student> listAll(){
+		return repo.findAll();
+		
+	}
+	
+	public List<Student> save(Student std) {
+		return repo.findAll();
+		
+	}
+	
+	public Student get(long id) {
+		return repo.findById(id).get();
+		
+	}
+	
+	public void delete(long id) {
+		repo.deleteById(id);
+	}
+
+}
